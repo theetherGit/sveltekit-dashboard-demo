c4989aada60f0083533eed6488143ac66f7db040
theetherGit <shivam@Shivams-Mac-mini.local>
2024-04-22 11:14:45 +0530

    cloudflare pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@ import {Button} from "$lib/components/ui/button";
 </script>
 
 <div class="grid content-center justify-center h-[85vh] w-full">
-    <h1>Yes, I know how to center a div</h1>
+    <h1>Yes, I know how to center a <code>div</code>.</h1>
     <Button href="/dashboard/daily-analytics">Navigation Demo</Button>
 </div>
 
